cfd94709e46761680343e92b735a4689d7c7f744
gpmn <gpmn@eden.com>
2024-04-22 13:27:38 +0800

    update gastip to 2

--- executor/const.go
+++ executor/const.go
@@ -4,5 +4,5 @@ const (
 	GAS_LIMIT        = 5000000
 	SUBMIT_GAS_LIMIT = 50000000
 	GWEI_UNIT        = 1000000000
-	GAS_TIP          = 1 //GWEI_UNIT / 100
+	GAS_TIP          = 2 //GWEI_UNIT / 100
 )
